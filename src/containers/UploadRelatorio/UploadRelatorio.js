464653f99c98faf4f3e3c1dd563544f1a2d14f6b
FelipeMassashi <felipemassashi@gmail.com>
2024-08-14 23:04:36 -0300

    feat: test CI

---
+++ src/containers/UploadRelatorio/UploadRelatorio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UploadRelatorio = () => {
+    return (
+        <div>
+			<h1>UploadRelatorio</h1>
+        </div>
+    );
+};
+
+export default UploadRelatorio;
